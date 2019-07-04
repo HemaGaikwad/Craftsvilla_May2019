0fc1c23fc06258499a174d111857eab62209a508
Zerobean <Zerobean@192.168.0.5>
2019-07-04 11:24:18 +0530

    Framework

---
+++ src/com/craftsvilla/pompages/HomePage.java
@@ -0,0 +1,97 @@
+package com.craftsvilla.pompages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.craftsvilla.generic.BasePage;
+
+public class HomePage extends BasePage
+{
+	
+	//Declarations
+	@FindBy(id="cartCount")
+	private WebElement LNK_cart;
+	
+	@FindBy(xpath="//span[text()='Sign In']")
+	private WebElement LNK_signIn;
+	
+	@FindBy(name="q")
+	private WebElement TXT_searchbox;
+	
+	@FindBy(xpath = "//a[@href=\"/womens-clothing/sarees/?MID=megamenu_sarees_seeall\"]")
+	private WebElement LNK_sarees;
+	
+	@FindBy(id="cv-logo")
+	private WebElement LNK_logo;
+	
+	@FindBy(xpath="//a[@href=\"/jewellery/necklaces/?MID=megamenu_jewellery_seeall\"]")
+	private WebElement LNK_jewellery;
+	
+	
+	//Initialize
+	public HomePage(WebDriver driver)
+	{
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Utilization
+	public void clickOnCartButton()
+	{
+		try
+		{
+			verifyElementPresent(LNK_cart);
+			LNK_cart.click();
+			log.info("clicked successfully on "+LNK_cart);
+		}
+		catch(Exception e)
+		{
+			log.error("Unable to click on "+LNK_cart+e);
+		}
+	}
+	
+	public void clickOnSignInLink()
+	{
+		try
+		{
+			verifyElementPresent(LNK_signIn);
+			LNK_signIn.click();
+			log.info("");
+		}
+		catch(Exception e)
+		{
+			log.error("");
+		}
+	}
+	
+	public void setSearchString(String searchVal)
+	{
+		try
+		{
+			verifyElementPresent(TXT_searchbox);
+			TXT_searchbox.sendKeys(searchVal);
+			log.info("");
+		}
+		catch(Exception e)
+		{
+			log.error("");
+		}
+	}
+
+	
+	public void goToSareesLink()
+	{
+		try
+		{
+			verifyElementPresent(LNK_signIn);
+			mouseHover(LNK_signIn);
+			log.info("");
+		}
+		catch(Exception e)
+		{
+			log.error("");
+		}
+	}
+}
